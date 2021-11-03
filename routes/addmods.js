95b32a5558a42a138b41e0d4e602ed95fb4d7d32
AbdulRehmanSayeed <mohammed.abdulrehman1610@gmail.com>
2021-11-02 21:51:21 -0500

    addmods table

--- routes/addmods.js
+++ routes/addmods.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  let query = req.query
+  var query=req.query
     console.log(`rows ${query.rows}`)
     console.log(`cols ${query.cols}`)
+    var rows=Number(query.rows)
+    var cols=Number(query.cols)
   res.render('addmods', { title: 'Add Mods', query: query });
 });
 
